5917437317797672fee54120440f8ab2a5db90fc
Luiz Eduardo <l.eduardo8316@gmail.com>
2020-08-30 19:06:46 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,6 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 class SignUpController {
-  handle(httpRequest: any): any {
-    
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
