c599dc284509e0b138e3756e450e335cfef61d01
Luiz Eduardo <l.eduardo8316@gmail.com>
2020-09-07 14:41:47 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/addAccount/dbAddAccount.spec.ts
+++ src/data/usecases/addAccount/dbAddAccount.spec.ts
@@ -118,4 +118,22 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'hashed_password',
+    })
+  })
 })
